21301d60d74a57c093cef0fed8d0bc8477b2a711
teklingl <teklingl@gmail.com>
2023-08-14 09:54:13 -0400

    Starter file(s).

---
+++ homework01/TestLove5.java
@@ -0,0 +1,25 @@
+// Delta College - CST 283 - Homework 1
+// Name:  
+
+public class TestLove5
+{
+   public static void main(String[] args)
+   {
+      
+      System.out.println( love5(5, 4) ); 
+      System.out.println( love5(1, 6) ); 
+      System.out.println( love5(3, 5) ); 
+      System.out.println( love5(9, 4) ); 
+
+      System.out.println( love5(4, 4) );    
+      System.out.println( love5(7, 3) );  
+         
+   }
+   
+   // Method love5
+   
+   
+   
+   
+}
+
