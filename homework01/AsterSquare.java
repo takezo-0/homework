29c7df800c2ed78da7378f6ba1d1d55f113ff112
teklingl <teklingl@gmail.com>
2023-08-15 09:44:13 -0400

    Starter file(s)

---
+++ homework01/AsterSquare.java
@@ -0,0 +1,34 @@
+// Delta College - CST 283 - Homework 1
+// Name:  
+
+public class AsterSquare
+{
+   public static void main(String[] args)
+   {
+      drawSquare(1);
+      System.out.println("------------");
+      drawSquare(2);
+      System.out.println("------------");
+      drawSquare(3);
+      System.out.println("------------");
+      drawSquare(5);
+      System.out.println("------------");
+      drawSquare(7);
+      System.out.println("------------");
+   }
+   
+   // Method drawSquare
+
+
+
+
+
+
+
+   }
+   
+   
+   
+   
+}
+
