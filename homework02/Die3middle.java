43626aa6e9d35cb010ec9e7e262fbca075b63cb4
teklingl <teklingl@gmail.com>
2023-08-18 13:14:40 -0400

    Starter file(s)

---
+++ homework02/Die3middle.java
@@ -0,0 +1,33 @@
+// Delta College - CST 283 - Homework 2
+// Name:
+
+public class Die3middle
+{
+   public static void main(String[] args)
+   {
+      // Test call to method
+      System.out.println(middle3of5dice());
+   }
+
+   // This method simulates throwing three dice and returns the middle-valided die throw
+   public static int middle3of5dice()
+   {
+
+         // Your code here
+
+      return 0;
+   }
+
+
+   // This method simulates throwing one die
+   public static int throwDie()
+   {
+      int theThrow = 0;
+
+        // Your code here
+
+      return theThrow;
+   }
+   
+}
+
