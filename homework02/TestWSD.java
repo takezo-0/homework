10472a949d7fe93da21c4e4444b45e2391be8efc
teklingl <teklingl@gmail.com>
2023-08-19 14:33:13 -0400

    Starter file(s)

---
+++ homework02/TestWSD.java
@@ -0,0 +1,24 @@
+// Delta College - CST 283 - Homework 1
+// Name:  
+
+public class TestWSD
+{
+   public static void main(String[] args)
+   {
+      System.out.println( wsd(1776) );
+   }
+
+   // -------------------------------------------------
+   // Method wsd(int)
+   public static int wsd(int number)
+   {
+
+      // Complete this method
+
+
+      return 0;
+   }
+   
+   
+   
+}
